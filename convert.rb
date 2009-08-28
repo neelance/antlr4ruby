1bc1dfdf7ff5c2d22f902c498bfe41b7ab79448e
neelance <mail@richard-musiol.de>
2009-08-28 22:21:37 +0200

    Deleted convert.rb.

--- convert.rb
+++
@@ -1,6 +0,0 @@
-require "converter/converter"
-
-dir = "#{File.dirname __FILE__}/antlr4ruby"
-controller = Java2Ruby::ConversionController.new
-controller.add_files "#{dir}/src", "#{dir}/lib", Dir.dir_glob("#{dir}/src", "**/*")
-controller.run $process_count
